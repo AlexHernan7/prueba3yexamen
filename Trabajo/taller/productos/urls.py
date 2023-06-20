0f5f257ecae1ee4b9170d2997bb92d38749a8939
vert1joint <diego.rrojas@live.cl>
2023-06-20 17:49:12 -0400

    agregando urls

--- Trabajo/taller/productos/urls.py
+++ Trabajo/taller/productos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index 
-
+from . import views
 
 urlpatterns = [
-    path('', index, name='name'),
+    path('', views.index),
+    path('mynosotros', views.nosotros, name='nosotros'),
 ]
+

3fa440030c846c0737d4991dae38dd526b7eb2e4
AlexHernan7 <alexmazuela.v@gmail.com>
2023-06-21 17:49:51 -0400

    crud

--- Trabajo/taller/productos/admin.py
+++ Trabajo/taller/productos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contacto
 
 # Register your models here.
+
+class ContactoAdmin(admin.ModelAdmin):
+    list_display="nombre","apellidoP","apellidoM","rut","direccion","email","phone","mensaje"
+
+admin.site.register(Contacto,ContactoAdmin)

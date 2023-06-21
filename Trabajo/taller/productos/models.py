cbb6dcc5968e5bf10f5f5baba780a5bb711b43df
AlexHernan7 <alexmazuela.v@gmail.com>
2023-06-21 15:42:19 -0400

    avances crud

--- Trabajo/taller/productos/models.py
+++ Trabajo/taller/productos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contacto(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellidoP=models.CharField(max_length=30)
+    apellidoM=models.CharField(max_length=30)
+    rut=models.CharField(max_length=10)
+    direccion=models.CharField(max_length=50)
+    email=models.CharField(max_length=60)
+    phone=models.IntegerField(max_length=11)
+    mensaje=models.CharField(max_length=300)
--- Trabajo/taller/productos/views.py
+++ Trabajo/taller/productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -15,3 +15,17 @@ def galeria(request):
 
 def vehiculos(request):
     return render(request, 'productos/vehiculos.html')
+
+
+def enviarMSJ(request):
+    a=request.POST['nombre']
+    b=request.POST['apellidoP']
+    c=request.POST['apellidoM']
+    d=request.POST['rut']
+    e=request.POST['direccion']
+    f=request.POST['email']
+    g=request.POST['phone']
+    h=request.POST['mensaje']
+    datos=enviarMSJ(nombre=a,apellidoP=b,apellidoM=c,rut=d,direccion=e,email=f,phone=g,mensaje=h)
+    datos.save()
+    return redirect("/")

ee2924e9740778ce06b59b2322ebca769459dd2b
vert1joint <diego.rrojas@live.cl>
2023-06-22 11:52:37 -0400

    superuser django

--- Trabajo/taller/productos/models.py
+++ Trabajo/taller/productos/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-class Contacto(models.Model):
+opciones_genero = [
+    [0,"null"],
+    [1,"Femenino"],
+    [2,"Masculino"]
+]
+
+class ContactoAdmin(models.Model):
     nombre=models.CharField(max_length=30)
     apellidoP=models.CharField(max_length=30)
     apellidoM=models.CharField(max_length=30)
-    rut=models.CharField(max_length=10)
+    rut=models.CharField(max_length=11)
+    genero=models.IntegerField(choices=opciones_genero)
     direccion=models.CharField(max_length=50)
-    email=models.CharField(max_length=60)
-    phone=models.IntegerField(max_length=11)
+    email=models.EmailField(max_length=60)
+    phone=models.CharField(max_length=11)
     mensaje=models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.apellidoP) 
